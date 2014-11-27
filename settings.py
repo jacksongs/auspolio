7c3fb5685dac45a09e67fbf44a332b05e8a04795
Jackson Gothe-Snape <jacksongs@gmail.com>
2014-11-28 08:23:59 +1100

    Updated timezone

--- settings.py
+++ settings.py
@@ -102,7 +102,7 @@ ALLOWED_HOSTS = []
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = "Canberra/Australia"
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
